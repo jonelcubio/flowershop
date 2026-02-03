cc6b50ed7d4de88af51ddc74e2fdcf1111c6d862
Jonelle Cubio <girlthatloveswriting@gmail.com>
2026-02-03 14:45:54 +0800

    🐂 under working progress, added the screenshot on assests for ui

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,19 @@
+export default function Landing() {
+  return(
+    <>
+    <div className="landing-con">
+      <div className="landing-left">
+        Lorem ipsum dolor sit.
+      </div>
+
+      <div className="landing-middle">
+        <img src="https://images.pexels.com/photos/18353268/pexels-photo-18353268/free-photo-of-close-up-of-pink-roses.jpeg?auto=compress&cs=tinysrgb&w=600" alt="bg-landing" className="landing-middle-img" />
+      </div>
+
+      <div className="landing-right">
+      </div>
+    </div>
+
+    </>
+  );
+}
